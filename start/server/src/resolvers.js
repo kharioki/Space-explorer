582b127270f284ac90a901645f10615b547e132d
tony <antolisah@gmail.com>
2021-09-21 01:04:17 +0300

    book and cancel trips resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -33,6 +33,39 @@ module.exports = {
         return user;
       }
     },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? 'Successfully booked trips'
+            : `the following launches couldn't be booked: ${launchIds.filter(
+              id => !results.includes(id),
+            )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result) {
+        return {
+          success: false,
+          message: 'Could not cancel trip',
+        };
+      }
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'Successfully cancelled trip',
+        launches: [launch],
+      };
+    },
   },
   Mission: {
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
