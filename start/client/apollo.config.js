eb20cc0f0343d18da166170296f10856389506d3
tony <antolisah@gmail.com>
2021-09-21 02:56:37 +0300

    install client dependencies

---
+++ start/client/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    name: 'Space Explore [web]',
+    service: 'space-explorer',
+  },
+};
---
+++ start/client/src/index.tsx
@@ -0,0 +1,24 @@
+import {
+  ApolloClient,
+  gql,
+  NormalizedCacheObject
+} from '@apollo/client';
+import { cache } from './cache';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql'
+});
+
+client.query({
+  query: gql`
+    query TestQuery {
+      launch(id: 56) {
+        id
+        mission {
+          name
+        }
+      }
+    }
+  `
+}).then(result => console.log(result));
