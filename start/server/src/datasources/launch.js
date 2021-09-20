a12bf5f23e39c8c66f2de4a87be7367c8d2b638d
tony <antolisah@gmail.com>
2021-09-20 22:57:33 +0300

    added getbyid methods

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -30,6 +30,17 @@ class LaunchAPI extends RESTDataSource {
       ? response.map(launch => this.launchReducer(launch))
       : [];
   }
+
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId })),
+    );
+  }
 }
 
 module.exports = LaunchAPI;
