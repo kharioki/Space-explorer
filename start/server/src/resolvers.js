6152dcd8253e3a40c518a4592aa528d5b9e3710c
tony <antolisah@gmail.com>
2021-09-21 00:45:36 +0300

    login mutation

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -25,6 +25,15 @@ module.exports = {
     launch: (_, { id }, { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
   },
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        user.token = Buffer.from(email).toString('base64');
+        return user;
+      }
+    },
+  },
   Mission: {
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
       return size === 'SMALL'
