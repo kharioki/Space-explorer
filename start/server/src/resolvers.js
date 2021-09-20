9d6ed72c33b0cdd0e30bac853f741763e2667ed2
tony <antolisah@gmail.com>
2021-09-21 00:18:49 +0300

    added mission, launch and user resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -3,5 +3,28 @@ module.exports = {
     launches: (_, __, { dataSources }) => dataSources.launchAPI.getAllLaunches(),
     launch: (_, { id }, { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
-  }
+  },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    },
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) => dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
 };
