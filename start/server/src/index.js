a62a0d5236fa387ebd97beb7fdd836a54e46c561
tony <antolisah@gmail.com>
2021-09-20 22:18:42 +0300

    define types, queries and mutations, initiate server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,12 @@
 require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    🚀 Server is ready 
+    Listening on port 4000
+  `);
+});
